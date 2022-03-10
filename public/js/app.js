73f95b451ea84c133ba22d858e584ba9d5c4f4f7
Ankit Mishra <ankit.mishra1089@gmail.com>
2022-03-10 13:34:49 +0530

    fixed all errors

--- public/js/app.js
+++ public/js/app.js
@@ -10,11 +10,10 @@ weatherForm.addEventListener("submit", (e) => {
   const location = searchElement.value;
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
-  fetch("http://localhost:3000/weather?address="+location).then((response) => {
+  fetch("/weather?address="+location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = 'Error';
-        messageTwo.textContent = data.error;
       } else {
         messageOne.textContent = data.location;
         messageTwo.textContent = "Temp: "+data.Temp;
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -5,10 +5,10 @@ const geocode = (address, callback) => {
       "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
       encodeURIComponent(address) +
       ".json?access_token=pk.eyJ1IjoiYW5raXQxMDg5IiwiYSI6ImNsMGdhMDhtZzA5aHgzam1zYmtqNTFjNzAifQ.wIh12iWZS8ZoVfONeU0CkA&limit=1";
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, {body}={}) => {
       if (error) {
         callback("Unable to connect to service!", undefined);
-      } else if (body.message) {
+      } else if (body.message||body.features.length===0) {
         callback("Unable to find location.", undefined);
       } else {
           callback(undefined, {
